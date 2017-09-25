49133d983095e7d1d2360c94acc888c8384f02bb
Eric Thomas <{ericjasonthomas}@users.noreply.github.com>
2017-09-25 13:44:36 -0400

    flex update

--- src/components/Section.test.js
+++ src/components/Section.test.js
@@ -2,21 +2,3 @@ import React from 'react';
 // import { Shallow } from 'enzyme'
 // import React, { Component } from 'react'
 import SiteCard from './SiteCard';
-
-// import ReactDOM from 'react-dom';
-//
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<Section />, div);
-// });
-// import { mount } from 'enzyme';
-// const wrapper = mount(<SiteCard/>);
-//
-describe('<SiteCard/>', () => {
-  if (!mountedSiteCard) {
-  mountedSiteCard = mount(
-    <SiteCard/>
-  );
-}
-return mountedSiteCard;
-}
