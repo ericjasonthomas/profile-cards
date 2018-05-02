ca997eb7d417a5c8badb4108d130123e447c4452
System Administrator <eric.thomas@jellyfish.net>
2018-05-02 17:44:50 -0400

    small update to package name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Theapp from './TheApp';
+import Theapp from './Theapp';
 import $ from 'jquery';
 import registerServiceWorker from './registerServiceWorker';
 
