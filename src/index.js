e93c6b3a2686ece45e06ab1244cf1a0c00a0b8de
EThomas <ericjasonthomas@gmail.com>
2019-03-19 15:29:33 -0400

    updated react app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Theapp from './Theapp';
-import $ from 'jquery';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Theapp />, document.getElementById('root'));
 registerServiceWorker();
-
-$(document).ready(function() {
-  $('.ms-core-overlay').removeClass('ms-core-overlay').addClass('backinblack');
-});
